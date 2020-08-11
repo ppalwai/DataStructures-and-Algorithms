f50333c4eab3f4a53fa8f1304988b982219348ca
ppalwai <praveen.palwai@gmail.com>
2020-08-11 18:03:19 -0400

    Add QuickSort with first element as pivot and last element as pivot

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -6,7 +6,7 @@ public class QuickSort {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(quickSort(new int[]{15, 5, 24, 8, 1, 3, 16, 10, 20})));
 
-        //System.out.println(Arrays.toString(partition(new int[]{15, 5, 24, 8, 1, 3, 16, 10, 20}, 0, 8)));
+        //System.out.println(partition(new int[]{15, 5, 24, 8, 1, 3, 16, 10, 20}, 0, 8));
     }
 
     static int[] quickSort(int[] arr) {
@@ -15,36 +15,61 @@ public class QuickSort {
 
     static int[] quickSort(int[] arr, int lb, int ub) {
         if(lb < ub) {
-            int pivotLoc = partition(arr, lb, ub);
-            quickSort(arr, lb, pivotLoc - 1);
-            quickSort(arr, pivotLoc + 1, ub);
+            int pivotIndex = partition(arr, lb, ub);
+            quickSort(arr, lb, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, ub);
         }
         return arr;
     }
 
     /**
-     *
+     * Use the first element as the pivot
      * @param arr
      * @param lb
      * @param ub
      */
+    /*
     static int partition(int[] arr, int lb, int ub) {
-       System.out.println("array before partition: " + Arrays.toString(arr) + " : " + lb + " : " + ub);
-       int pivot = arr[lb], start = lb, end = ub;
-       int temp = 0;
-       while(start < end) {
-           while(arr[start] <= pivot) start++;
-           while(arr[end] > pivot) end--;
-           if(start < end) {
-               temp = arr[start];
-               arr[start] = arr[end];
-               arr[end] = temp;
-           }
-       }
-       temp = arr[lb];
-       arr[lb] = arr[end];
-       arr[end] = temp;
-       System.out.println("array after parition: " + Arrays.toString(arr));
-       return end;
+        System.out.println("array before partition: " + Arrays.toString(arr) + " : " + lb + " : " + ub);
+        int start = lb, end = ub;
+        while(start < end) {
+            while (start < ub && arr[start] <= arr[lb]) start++;
+            while (end > lb && arr[end] > arr[lb]) end--;
+            if(start < end) {
+                swap(arr, start, end);
+            }
+        }
+        swap(arr, lb, end);
+        System.out.println("array after parition: " + Arrays.toString(arr));
+
+        return end;
+    }
+    */
+
+    /**
+     * Use last element as the pivot
+     * @param arr
+     * @param lb
+     * @param ub
+     * @return
+     */
+    static int partition(int[] arr, int lb, int ub) {
+        System.out.println("array before partition: " + Arrays.toString(arr) + " : " + lb + " : " + ub);
+        int pivotIndex = lb;
+        for(int i = lb; i < ub; i++) {
+            if(arr[i] <= arr[ub]) {
+                swap(arr, i, pivotIndex);
+                pivotIndex++;
+            }
+        }
+        swap(arr, ub, pivotIndex);
+        System.out.println("array after parition: " + Arrays.toString(arr));
+        return pivotIndex;
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
